304acf825d1a9e49b905996db313b4d44711d2f1
Tyler Starr <starrtyler88@gmail.com>
2025-09-01 15:24:45 -0700

    try out gnome extensions

--- build_files/desktop/gnome.sh
+++ build_files/desktop/gnome.sh
@@ -2,6 +2,17 @@
 
 set -ouex pipefail
 
+install_gnome_extension(){
+    wget "$1" -O /tmp/ext.zip
+    unzip -j /tmp/ext.zip -d "/usr/share/gnome-shell/extensions/$2"
+}
+
+# gnome extensions
+rm -R /usr/share/gnome-shell/extensions/*
+install_gnome_extension https://github.com/starr-dusT/gnome-set-panel-monitor/archive/refs/heads/master.zip gnome-set-panel-monitor@tstarr.us
+chmod -R 644 /usr/share/gnome-shell/extensions
+
+# dconf setup
 tee /etc/dconf/db/local.d/02-test <<'EOF'
 [org/gnome/shell]
 favorite-apps = ['chromium-browser.desktop']
